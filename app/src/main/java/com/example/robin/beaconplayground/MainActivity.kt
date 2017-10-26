9b4e9cbdb029567696b773a700b4563c64ac6f63
Robin Edbom <robin.edbom@gmail.com>
2017-10-27 00:49:03 +0200

    Added beacon distance reader

--- app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
+++ app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.robin.beaconplayground
 
+import android.Manifest
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.FragmentActivity
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.altbeacon.beacon.*
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), BeaconConsumer {
 
+    private lateinit var _beaconManager: BeaconManager
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +32,17 @@ class MainActivity : FragmentActivity() {
 
         viewPager.adapter = MyPageAdapter(supportFragmentManager)
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Needs location")
+            alert.setPositiveButton("Nice!", null)
+            alert.show()
+        }
+
+        _beaconManager = BeaconManager.getInstanceForApplication(this)
+        _beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT))
+
+        _beaconManager.bind(this)
         registerReceiver(getListener(), IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
     }
 
@@ -35,6 +53,29 @@ class MainActivity : FragmentActivity() {
     override fun onResume() {
         super.onResume()
         verifyBluetoothState(BluetoothAdapter.getDefaultAdapter().state)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _beaconManager.unbind(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBeaconServiceConnect() {
+        _beaconManager.addRangeNotifier({ beacons: MutableCollection<Beacon>, region: Region ->
+            if (beacons.size > 0) {
+                val distance = "The first beacon I see is about ${"%.2f".format(beacons.iterator().next().distance)} meters away"
+                    runOnUiThread({
+                        getTextView().text = distance;
+                    })
+                Log.i("Beacon distance", distance)
+            }
+        })
+
+        try {
+            _beaconManager.startRangingBeaconsInRegion(Region("eff799ba-e5bc-493b-9995-10a38bedc784", null, null, null))
+        } catch (e: Exception) {
+        }
     }
 
     private fun getListener(): BroadcastReceiver {
