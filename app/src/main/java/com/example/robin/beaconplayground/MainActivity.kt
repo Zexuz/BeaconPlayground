fe098ea3fb561709e115cb3e659938556fc5c9df
Robin Edbom <robin.edbom@gmail.com>
2017-10-26 23:12:51 +0200

    Added a swipe view and fragment

--- app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
+++ app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
@@ -5,13 +5,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +35,57 @@ class MainActivity : AppCompatActivity() {
                 adapter.enable()
         })
 
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        pager.adapter = MyPagerAdapter(supportFragmentManager)
+
         registerReceiver(getListener(), IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
+    }
+
+    class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getCount(): Int {
+            return 2
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> FirstFragment.newInstace("Blue")
+                else -> FirstFragment.newInstace("Red")
+            }
+        }
+    }
+
+    class FirstFragment : Fragment() {
+
+
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val view = inflater?.inflate(R.layout.first_frag, container, false)
+
+            val textView = view?.findViewById<TextView>(R.id.infoText)
+
+            val colorString = arguments.getString("msg")
+            val color = when (colorString) {
+                "Blue" -> Color.BLUE
+                else -> Color.RED
+            }
+
+            view?.setBackgroundColor(color)
+
+            textView?.text = colorString
+
+            return view
+        }
+
+        companion object {
+            fun newInstace(text: String): FirstFragment {
+                val fragment = FirstFragment()
+
+                val bundle = Bundle()
+                bundle.putString("msg", text)
+
+                fragment.arguments = bundle
+                return fragment
+            }
+        }
     }
 
     private fun getTextView(): TextView {
