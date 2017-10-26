27d2c9a87dafa5a3bb8ef574eecdd00119316cab
Robin Edbom <robin.edbom@gmail.com>
2017-10-27 01:30:23 +0200

    This should make it feel alot more "responsive" in the distace.

--- app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
+++ app/src/main/java/com/example/robin/beaconplayground/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.altbeacon.beacon.*
+import org.altbeacon.beacon.service.RangedBeacon
 
 class MainActivity : FragmentActivity(), BeaconConsumer {
 
@@ -39,6 +40,7 @@ class MainActivity : FragmentActivity(), BeaconConsumer {
             alert.show()
         }
 
+        RangedBeacon.setSampleExpirationMilliseconds(5000) //https://stackoverflow.com/questions/25520713/how-to-get-faster-ranging-responses-with-altbeacon
         _beaconManager = BeaconManager.getInstanceForApplication(this)
         _beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT))
 
